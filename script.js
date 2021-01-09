4238af9b15805e86e6ac465b1217e1c3d14878f4
RodasNega <rodas.nega@ocsbstudent.ca>
2021-01-09 17:15:45 +0000

    modified

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 document.getElementById('product').addEventListener('click', calculateEquation)
 
 function calculateEquation () {
-  let digit1 = document.getElementById('multiplicand').value; 
+  const digit1 = document.getElementById('multiplicand').value
   // variable assigned to the first text box
-  let digit2 = document.getElementById('multiplier').value;
+  let digit2 = document.getElementById('multiplier').value
   // variable assigned to the second text box
-  let result = 0;
-  while (digit2 != 0) {
+  let result = 0
+  while (digit2 !== 0) {
   // a loop to check when digit2 is not equal to 0
     if (digit2 > 0) {
-      digit2--;
+      digit2--
     } else if (digit2 < 0) {
-      digit2++;
+      digit2++
     }
-  // conditions that check to see if digit2 is greater than 0 then decrease it by that increment other if it's less than 0 then increase it by that increment
-  result += parseInt(digit1);
-  // adds digit1 to itself by the number of digit2 times
+    // conditions that check to see if digit2 is greater than 0 then decrease it by that increment other if it's less than 0 then increase it by that increment           
+  result += parseInt(digit1)
+    // adds digit1 to itself by the number of digit2 times
   }
-  alert(result);
+  alert(result)
   // returns the final result value
 }
