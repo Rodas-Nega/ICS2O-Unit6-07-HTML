e214848fedf58ab3aa2e82d27e9b6ccfd0e67e2e
RodasNega <rodas.nega@ocsbstudent.ca>
2021-01-09 17:07:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict'
+
+document.getElementById('product').addEventListener('click', calculateEquation)
+
+function calculateEquation () {
+  let digit1 = document.getElementById('multiplicand').value; 
+  // variable assigned to the first text box
+  let digit2 = document.getElementById('multiplier').value;
+  // variable assigned to the second text box
+  let result = 0;
+  while (digit2 != 0) {
+  // a loop to check when digit2 is not equal to 0
+    if (digit2 > 0) {
+      digit2--;
+    } else if (digit2 < 0) {
+      digit2++;
+    }
+  // conditions that check to see if digit2 is greater than 0 then decrease it by that increment other if it's less than 0 then increase it by that increment
+  result += parseInt(digit1);
+  // adds digit1 to itself by the number of digit2 times
+  }
+  alert(result);
+  // returns the final result value
+}
